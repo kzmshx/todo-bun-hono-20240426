f707f66985d44108aed172470d8951632392b150
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-05-03 22:34:27 +0900

    refactor: Remove unused code from `Container`

--- src/libs/hono/dependency-injection/container.ts
+++ src/libs/hono/dependency-injection/container.ts
@@ -8,21 +8,11 @@ type ServiceMap = {
 };
 
 export class Container implements ServiceMap {
-  constructor(public env: Env) {}
+  constructor(public readonly env: Env) {}
 
   private _prisma?: PrismaClient;
+
   get prisma() {
     return (this._prisma ??= prisma);
-  }
-  set prisma(value: PrismaClient) {
-    this._prisma = value;
-  }
-
-  get<T extends keyof ServiceMap>(name: T): ServiceMap[T] {
-    return this[name];
-  }
-
-  set<T extends keyof ServiceMap>(name: T, value: ServiceMap[T]) {
-    (this as ServiceMap)[name] = value;
   }
 }
