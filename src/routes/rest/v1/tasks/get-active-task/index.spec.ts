c45e02382d76c5efc3178b5346d2a6c30815ddc3
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-05-03 22:33:48 +0900

    test: Add a test case for `get-active-task`

--- src/routes/rest/v1/tasks/get-active-task/index.spec.ts
+++ src/routes/rest/v1/tasks/get-active-task/index.spec.ts
@@ -6,21 +6,33 @@ import { ulid } from "ulid";
 describe("GET /rest/v1/tasks/:id", () => {
   test("200 OK", async () => {
     // Arrange
-    const task = await TaskFactory.create();
+    const activeTask = await TaskFactory.create({ isCompleted: false });
     // Act
-    const response = await app.request(`/rest/v1/tasks/${task.id}`);
+    const response = await app.request(`/rest/v1/tasks/${activeTask.id}`);
     // Assert
     expect(response.status).toBe(200);
     expect(await response.json()).toMatchObject({
-      id: task.id,
-      content: task.content,
-      description: task.description,
-      is_completed: task.isCompleted,
-      created_at: task.createdAt.toISOString(),
+      id: activeTask.id,
+      content: activeTask.content,
+      description: activeTask.description,
+      is_completed: activeTask.isCompleted,
+      created_at: activeTask.createdAt.toISOString(),
     });
   });
 
-  test("404 Not Found", async () => {
+  test("404 Not Found - Completed Task", async () => {
+    // Arrange
+    const completedTask = await TaskFactory.create({ isCompleted: true });
+    // Act
+    const response = await app.request(`/rest/v1/tasks/${completedTask.id}`);
+    // Assert
+    expect(response.status).toBe(404);
+    expect(await response.json()).toMatchObject({
+      message: "Task not found",
+    });
+  });
+
+  test("404 Not Found - Non Existent Task", async () => {
     // Act
     const response = await app.request(`/rest/v1/tasks/${ulid()}`);
     // Assert
